dbe0943efaf085d591b5d2fe987561c4058950ba
Marcin <marcin.t.kozak@gmail.com>
2023-06-13 21:53:56 +0100

    done

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+
+        boolean leapYear;
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            leapYear = true;
+        } else {
+            leapYear = false;
+        }
     }
 }
